3481a81fa5b24c8ca819594b0ab056fa041bc042
fri5nd0 <125700314+fri5nd0@users.noreply.github.com>
2023-10-06 18:31:06 +0100

    K

--- world.gd
+++ world.gd
@@ -1,5 +1,5 @@
 extends Node
-var scoreboard = {}
+@export var scoreboard = {}
 @onready var menu = $CanvasLayer/MainMenu
 @onready var address = $CanvasLayer/MainMenu/MC/Options/address_input
 @onready var map = $Node3D
@@ -16,33 +16,29 @@ func _on_host_b_pressed():
 		multiplayer.multiplayer_peer = enet_p
 		multiplayer.peer_connected.connect(add_player)
 		var peerID = enet_p.get_unique_id()
-		players[peerID] = gameName.text
-		print('dict of:',gameName.text,players)
 		add_player(1)
 
-
+func _input(event):
+	if Input.is_action_just_pressed("Jump"):
+		print(players)
+		
 func _on_join_b_pressed():
 	if gameName.text:
 		print('joinB pressed')
 		menu.hide()
 		enet_p.create_client('localhost',port)
 		multiplayer.multiplayer_peer = enet_p
-		var peerID = enet_p.get_unique_id()
-		players[peerID] = gameName.text
-		print('dict of:',gameName.text,players)
 		
 @rpc("any_peer")	
 func add_player(peer_id):
-	var PName = gameName.text
-	if enet_p.get_unique_id() == 1:
-		add_player.rpc(peer_id)
-		var player = Player.instantiate()
-		player.name = str(peer_id)
-		add_child(player, true)
-		for p in scoreboard.keys():
-			add_player.rpc_id(peer_id, int(p))
+	var player = Player.instantiate()
+	player.name = str(peer_id)
+	add_child(player, true)
 	scoreboard[str(peer_id)] = 0
-
+	if peer_id == 1:
+		players[peer_id] = gameName.text
+	else:
+		players[peer_id] = ''
 func findSafeSpawn():
 	var safeQuadrant: int
 	var Q1 = map.get_node('Quadrant1')
@@ -84,10 +80,9 @@ func setPlayerNames(PName,peer_id):
 		players[PName] = str(peer_id)
 
 @rpc("any_peer")
-func addPlayertodict(peerid,gName):
+func addPlayertodict(peerid):
+	var gName = gameName.text
 	players[str(peerid)] = gName 
 
-@rpc("any_peer")
-func setPlayersDict(dict):
-	for key in dict.keys():
-		players[key] = dict[key]
+func getPlayerName(): 
+	pass
